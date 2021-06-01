21263b9044a02581c4105a3cadd38c46bfc92904
James Kwakye <JamesK@Konsise.com>
2021-06-01 21:47:40 +0200

    This is the version 1 of the project

---
+++ SARSProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SARSProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello James!");
+        }
+    }
+}
